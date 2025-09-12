a06a644230eb3b369a57195bd2b16f608b1eaf6d
FurqanRafi <furqanf64123@gmail.com>
2025-09-12 15:36:34 +0500

    g

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,8 +27,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased scroll-smooth`}
       >
+
         <Header />
         {children}
         <Footer />
